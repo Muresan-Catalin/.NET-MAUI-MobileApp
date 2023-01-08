2319ed6988526b39c2f338d04cd1863bf3c90314
catam <catam@CATA>
2023-01-08 21:19:51 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MobileApp.Data;
+using System.IO;
+
+
+namespace MobileApp;
+
+public partial class App : Application
+{
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database {
+        get {
+            if (database == null) {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
+    public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
